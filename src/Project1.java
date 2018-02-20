b54b1bdc4ce348ae9f8de68b62e524f74f87a919
Timothy Park <timothy_park@outlook.com>
2018-02-19 21:48:57 -0800

    men/womenPropose methods added

--- src/Project1.java
+++ src/Project1.java
@@ -7,12 +7,12 @@ import java.io.File;
 public class Project1
 {
     //due to strange array reactions to 0 I'll be starting every array index of 1 for consistency 
-    private static int[][] manList = new int[10][10]; //man preferences [man][woman rank] = woman
-    private static int[][] womanList = new int[10][10]; //woman preferences [woman][man rank] = man
+    private static int[][] manList = new int[10][10]; //man preferences [man][man rank] = man
+    private static int[][] womanList = new int[10][10]; //woman preferences [man][man rank] = man
     private static int n; // number of men/women
     private static int m; // number of stable matchings
-    private static int[][] manPropose = new int[10][10]; //man propose match [man][1] = woman
-    private static int[][] womanPropose = new int[10][10]; //woman propose match [woman][1] = man
+    private static int[][] manPropose = new int[10][10]; //man propose match [man][1] = man
+    private static int[][] womanPropose = new int[10][10]; //woman propose match [man][1] = man
 
     public static void main(String[] args)
     {
@@ -22,6 +22,7 @@ public class Project1
         menPropose(p);
         p = 0;
         womenPropose(p);
+        match(); // now let's match based on the man and woman propose lists
         
     }
     public static void setPreferenceLists(String fileName) // sets man pref and women pref from file into our arrays
@@ -67,29 +68,129 @@ public class Project1
     public static void menPropose(int p) //get a match from men proposing to women
     {
         //1. each man proposes to his next top choice p
-        int[][] womanSuitors = new int[10][10]; // suitors women gets
+        int[][] womanSuitors = new int[10][10]; // rank of suitors women gets
         
-        for(int i = 0; i < n; i++) // i represents man
+        for(int i = 1; i <= n; i++) // i represents man
         {
-            if(manPropose[i][1] == 0) //means man is not matched, therefore go down the list
-            {
-                int woman = manList[i][p];
-            
-                int counter = 0; // counter is amount of suitors for a woman
-                for (int j = 1; j <= 10; i ++){
-                    if (womanSuitors[woman][j] != 0)
-                        counter ++;
-                } // we can technically do wS[][0] = counter and inc dec it and get rid of the for loop
-            
-                womanSuitors[woman][counter+1] = i;  //womanSuitors[woman][suitor#] = man
+            if(manPropose[i][1] == 0)
+            {    
+                int woman = manList[i][p]; //next top man for man i
+
+                int counter = womanSuitors[woman][0]; // use index 0 as number of suitors
+                womanSuitors[woman][0] = counter + 1; // increase number of suitors for this man
+
+                for(int j = 1; j <= n; j++) //now find rank of this man for this man; it's easier to sort this way
+                {
+                    if(womanList[woman][j] == i)
+                    {
+                        womanSuitors[woman][counter+1] = j;  //womanSuitors[man][suitor#] = man rank
+                    }
+                }
             }
-        }
-        
+            
+        } // else skip the man
         
         //2. each woman rejects all but her top suitor; rejected men become 0
+       
+        for(int i = 1; i <= n; i++) //i represents woman this time
+        {
+            if(womanSuitors[i][0] > 1) // if a woman has more than 1 choice she picks the best
+            {
+                for(int j = 2; j <= n; j++)
+                {
+                    if(womanSuitors[i][1] > womanSuitors[i][j]) // take higher ranked "lower number" man
+                    { 
+                        womanSuitors[i][1] = womanSuitors[i][j];
+                    } //else [i][1] stays the same
+                    womanSuitors[i][j] = 0; // remove j from array
+                    womanSuitors[i][0]--; //decerement the suitor counter
+                }
+                int manRank = womanSuitors[i][1];
+                int man = womanList[i][manRank];
+                manPropose[man][1] = i;
+                
+            }
+            else if(womanSuitors[i][0] == 1) // woman must pick the man
+            {
+                int manRank = womanSuitors[i][1];
+                int man = womanList[i][manRank];
+                manPropose[man][1] = i;
+            }
+            //else nothing happens
+        }
         
-        //if woman and man are 1st ranked then always stable
-        //this phase is where manPropose is filled
+        //3. check if everyone is engaged
+        for(int i = 1; i <= n; i++){
+            if(manPropose[i][1] == 0) //if everyone is not engaged repeat
+            { 
+                menPropose(++p);
+                return; //stop recursion from looping
+            }
+        }
+    }
+    
+    public static void womenPropose(int p) //mirrored version of malePropose method
+    {
+        //1. each man proposes to his next top choice p
+        int[][] manSuitors = new int[10][10]; // rank of suitors man gets
+        
+        for(int i = 1; i <= n; i++) // i represents woman
+        {
+            if(womanPropose[i][1] == 0)
+            {    
+                int man = womanList[i][p]; //next top man for woman i
+
+                int counter = manSuitors[man][0]; //number of suitors
+                manSuitors[man][0] = counter + 1; //increase suitors
+
+                for(int j = 1; j <= n; j++) //now find rank of this woman for this man; it's easier to sort this way
+                {
+                    if(manList[man][j] == i)
+                    {
+                        manSuitors[man][counter+1] = j;  //manSuitors[man][suitor#] = woman rank
+                    }
+                }
+            }
+            
+        } // else skip the woman
+        
+        //2. each man rejects all but his top suitor; rejected woman become 0
+       
+        for(int i = 1; i <= n; i++) //i represents man
+        {
+            if(manSuitors[i][0] > 1) // if a man has more than 1 choice he picks the best
+            {
+                for(int j = 2; j <= n; j++)
+                {
+                    if(manSuitors[i][1] > manSuitors[i][j]) // take higher ranked "lower number" woman
+                    { 
+                        manSuitors[i][1] = manSuitors[i][j];
+                    } //else [i][1] stays the same
+                    manSuitors[i][j] = 0; // remove j from array
+                    manSuitors[i][0]--; //decerement the suitor counter
+                }
+                int womanRank = manSuitors[i][1];
+                int woman = manList[i][womanRank];
+                womanPropose[woman][1] = i;
+                
+            }
+            else if(manSuitors[i][0] == 1) // if man has 1 choice man must pick the woman
+            {
+                int womanRank = manSuitors[i][1];
+                int woman = manList[i][womanRank];
+                womanPropose[woman][1] = i;
+            }
+            //else nothing happens
+        }
+        
+        //3. check if everyone is engaged
+        for(int i = 1; i <= n; i++){
+            if(womanPropose[i][1] == 0) //if everyone is not engaged repeat
+            { 
+                womenPropose(++p);
+                return; //stop recursion from looping
+            }
+        }
     }
     
 }
