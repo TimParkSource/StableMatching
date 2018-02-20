a82f24ff7e2f0b468b404ed17c1c5df9d13d37e2
Timothy Park <timothy_park@outlook.com>
2018-02-19 17:14:38 -0800

    Did work on counting suitors and other things

--- src/Project1.java
+++ src/Project1.java
@@ -6,12 +6,13 @@ import java.io.File;
 
 public class Project1
 {
-    private static int[][] manList = new int[10][10]; //man preferences list
-    private static int[][] womanList = new int[10][10]; //woman preferences list
-    private static int n; // number of men or women
+    //due to strange array reactions to 0 I'll be starting every array index of 1 for consistency 
+    private static int[][] manList = new int[10][10]; //man preferences [man][woman rank] = woman
+    private static int[][] womanList = new int[10][10]; //woman preferences [woman][man rank] = man
+    private static int n; // number of men/women
     private static int m; // number of stable matchings
-    private static int[][] manPropose = new int[10][10]; //man propose match
-    private static int[][] womanPropose = new int[10][10]; //woman propose match
+    private static int[][] manPropose = new int[10][10]; //man propose match [man][1] = woman
+    private static int[][] womanPropose = new int[10][10]; //woman propose match [woman][1] = man
 
     public static void main(String[] args)
     {
@@ -19,6 +20,8 @@ public class Project1
         setPreferenceLists("input1.txt");
         int p = 0;
         menPropose(p);
+        p = 0;
+        womenPropose(p);
         
     }
     public static void setPreferenceLists(String fileName) // sets man pref and women pref from file into our arrays
@@ -26,7 +29,7 @@ public class Project1
         File file = new File(fileName);
         try {
             int i = 0;
-            int j = 0;
+            int j;
 
             Scanner sc = new Scanner(file);
 
@@ -39,17 +42,17 @@ public class Project1
                 }
                 else if(i <= n && i > 0) //set manList
                 {
-                    for(j = 0; j < n; j++)
+                    for(j = 1; j <= n; j++)
                     {
-                        manList[i-1][j]= sc.nextInt();  // i-1 tomake men start at 0
+                        manList[i][j]= sc.nextInt();  // i-1 tomake men start at 0
                     }
                     i++;
                 }
                 else if( i > n && i <= 2*n ) //set womanList
                 {
-                    for(j = 0; j < n; j++)
+                    for(j = 1; j <= n; j++)
                     {
-                        womanList[i-1-n][j]= sc.nextInt();  //i-1-n tomake women start at 0
+                        womanList[i-n][j]= sc.nextInt();  //i-1-n tomake women start at 0
                     }
                     i++;
                 }
@@ -63,19 +66,30 @@ public class Project1
     
     public static void menPropose(int p) //get a match from men proposing to women
     {
-        //each man proposes to his top choice
+        //1. each man proposes to his next top choice p
         int[][] womanSuitors = new int[10][10]; // suitors women gets
-        int z; // figure outt how to keep track of z
+        
         for(int i = 0; i < n; i++) // i represents man
         {
-            womanSuitors[manList[i][p]][z] = i;  //womanSuitors[woman][suitor#] = man
+            if(manPropose[i][1] == 0) //means man is not matched, therefore go down the list
+            {
+                int woman = manList[i][p];
             
+                int counter = 0; // counter is amount of suitors for a woman
+                for (int j = 1; j <= 10; i ++){
+                    if (womanSuitors[woman][j] != 0)
+                        counter ++;
+                } // we can technically do wS[][0] = counter and inc dec it and get rid of the for loop
+            
+                womanSuitors[woman][counter+1] = i;  //womanSuitors[woman][suitor#] = man
+            }
         }
-        int counter = 0;
-        for (int i = 0; i < womanSuitors.length; i ++){
-            if (womanSuitors[i] != null)
-            counter ++;
-        }
-        //each woman rejects all but her top suitor
+        
+        
+        //2. each woman rejects all but her top suitor; rejected men become 0
+        
+        //if woman and man are 1st ranked then always stable
+        //this phase is where manPropose is filled
     }
+    
 }
